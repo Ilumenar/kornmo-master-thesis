70e2b489aef2cb5f9b61390e253e5f107d0a34da
Sigurd Jacobsen <sigurd@grandalsmyra.com>
2022-02-23 15:17:15 +0100

    Finished script for creating lookup table for what fields in soil quality intersects with disposed properties

--- src/data_handling/extract_fields.py
+++ src/data_handling/extract_fields.py
@@ -2,40 +2,40 @@ import pandas as pd
 import geopandas as gpd
 import numpy as np
 from tqdm import tqdm
-from shapely.geometry import box, mapping, Polygon
+from shapely.ops import transform
 from shapely import wkt
-import matplotlib.pyplot as plt
-import os, sys
+import os
+import pyproj
 from satellite_images import read_sat_images_file
-from fiona import collection
-from fiona.crs import from_epsg
-from pyproj import Proj, Transformer, transform
-from utils import boundingBox
 
+data_location = "../kornmo-data-files/raw-data"
 
-data_location = "E:/Universitetet i Agder/Mikkel Andreas Kvande - kornmo-data-files/raw-data"
-
-def get_farmer_elevation(nrows=None):
-    farmer_elevation = pd.read_csv(os.path.join(data_location, 'farm-information/farmer_elevation.csv'), delimiter=',', nrows=nrows)
-    columns_to_keep = ['orgnr', 'lat', 'lng', 'commune_id']
-    farmer_elevation = farmer_elevation.filter(columns_to_keep)
-    farmer_elevation['orgnr'] = farmer_elevation['orgnr'].astype(str)
-    farmer_elevation['lat'] = farmer_elevation['lat'].astype(float)
-    farmer_elevation['lng'] = farmer_elevation['lng'].astype(float)
-    farmer_elevation['commune_id'] = farmer_elevation['commune_id'].astype(int)
-    return farmer_elevation
+def get_farmer_centroid(nrows=None):
+    farmer_centroid = pd.read_csv(os.path.join(data_location, 'farm-information/centroid_coordinates_new.csv'), delimiter=',', nrows=nrows)
+    columns_to_keep = ['orgnr', 'latitude', 'longitude', 'kommunenr']
+    farmer_centroid = farmer_centroid.filter(columns_to_keep)
+    farmer_centroid = farmer_centroid.dropna()
+    farmer_centroid['orgnr'] = farmer_centroid['orgnr'].astype(str)
+    farmer_centroid['latitude'] = farmer_centroid['latitude'].astype(float)
+    farmer_centroid['longitude'] = farmer_centroid['longitude'].astype(float)
+    farmer_centroid['kommunenr'] = farmer_centroid['kommunenr'].astype(int)
+    return farmer_centroid
 
 def get_polygon_data(nrows=None):
-    
-    soilquality = pd.read_csv(os.path.join(data_location, 'soil-data/jordsmonn_geometry.csv'), dtype=str)
+    soilquality = pd.read_csv(os.path.join(data_location, 'soil-data/jordsmonn_geometry.csv'), dtype=str, nrows=nrows)
+    soilquality = soilquality.dropna()
     soilquality['geometry'] = soilquality['geometry'].apply(wkt.loads)
     geo_soilquality = gpd.GeoDataFrame(soilquality, crs='epsg:4326')
-    geo_soilquality['KOMID'] = geo_soilquality['KOMID'].astype(int)
-    
-    
+    geo_soilquality['municipal_nr'] = geo_soilquality['municipal_nr'].astype(int)
     return geo_soilquality
 
-#Combines the org numbers from both files
+def get_disp_eiendommer():
+    disp_eien = gpd.read_file(os.path.join(data_location, 'farm-information/farm-properties/disposed-properties-previous-students/disponerte_eiendommer.gpkg'), layer='disponerte_eiendommer')
+    disp_eien = disp_eien.dropna()
+    disp_eien.drop_duplicates(['orgnr', 'geometry'], keep='first', inplace=True)
+    disp_eien['orgnr'] = disp_eien['orgnr'].astype(str)
+    return disp_eien
+
 def get_combined_satellite_data():
     sat_images0 = read_sat_images_file('sentinel_100x100_0.h5')
     sat_images1 = read_sat_images_file('sentinel_100x100_1.h5')
@@ -48,84 +48,79 @@ def get_combined_satellite_data():
 
     return result
 
+def filter_data():
+    #Getting datasets
+    farmer_centroid = get_farmer_centroid()
+    field_data = get_polygon_data()
+    disp_eien = get_disp_eiendommer()
 
-def get_orgnrs_by_municipal(municipal_nr, orgnr_dataframe):
-    return orgnr_dataframe.loc[orgnr_dataframe['commune_id'] == municipal_nr]
+    #Only keep data that have common orgnrs
+    sat_orgnr = np.array(get_combined_satellite_data())
+    farm_orgnr = np.array(list(disp_eien['orgnr']))
+    intersection = np.intersect1d(sat_orgnr, farm_orgnr)
+    filtered_disp_eien = disp_eien[disp_eien['orgnr'].isin(intersection)]
 
+    # print(f"Amount of fields from disposed properties: {filtered_disp_eien.shape}")
+    # print(f"Amount of organisation numbers from satellite data: {len(filtered_satellite_data)}")
+    # print(f"Amount of fields from jordsmonn: {field_data.shape}")
 
-def get_polygons_by_municipal(polygons, municipal_nr):
-    polygons_by_muni = polygons.loc[polygons['KOMID'] == municipal_nr]
-    polygons_list = list(polygons_by_muni['geometry'])
-    return polygons_list
+    #Only keep data that have common municipal numbers
+    municipal_nrs, idxs_to_remove = [], []
+    orgnrs_to_check = list(set(farmer_centroid['orgnr'].tolist()))
+    for index, row in tqdm(filtered_disp_eien.iterrows(), total=filtered_disp_eien.shape[0]):
+        if row['orgnr'] in orgnrs_to_check:
+            municipal_nrs.append(farmer_centroid.loc[farmer_centroid['orgnr'] == row['orgnr']]['kommunenr'].iloc[0])
+        else:
+            idxs_to_remove.append(index)
+    filtered_disp_eien = filtered_disp_eien.drop(idxs_to_remove)
+    #Adds municipal numbers to disposed properties
+    filtered_disp_eien.insert(1, "municipal_nr", municipal_nrs)
+
+    return field_data, filtered_disp_eien
+
+def filter_by_municipal(dataframe, municipal_nr):
+    return dataframe.loc[dataframe['municipal_nr'] == municipal_nr]
+
+def convert_crs(polygons):
+    project = pyproj.Transformer.from_proj(pyproj.Proj('epsg:25833'), pyproj.Proj('epsg:4326'), always_xy=True)
+    return [transform(project.transform, poly) for poly in polygons]
+
+def extract_orgnr_per_field(field_data, filtered_disp_eien):
+    intersections_df = []
+    municipal_nrs = list(set(filtered_disp_eien['municipal_nr'].tolist()))
+
+    for municipal_nr in tqdm(municipal_nrs):
+
+        filtered_disp = filter_by_municipal(filtered_disp_eien, municipal_nr)
+        filtered_fields = filter_by_municipal(field_data, municipal_nr)
+
+        polygons_fields = filtered_fields['geometry'].tolist()
+        polygons_disp = filtered_disp['geometry'].tolist()
+        id_fields = filtered_fields['id'].tolist()
+        orgnr_disp = filtered_disp['orgnr'].tolist()
+        
+        for i, poly_field in enumerate(polygons_fields):
+            disp_orgnrs = []
+            for j, poly_disp in enumerate(polygons_disp):
+                
+                if poly_disp.intersects(poly_field):
+                    disp_orgnrs.append(orgnr_disp[j])
+            if len(disp_orgnrs) > 0:
+                intersections_df.append([id_fields[i], municipal_nr, disp_orgnrs])
+
+    intersections_df = pd.DataFrame(intersections_df, columns=['field_id', 'municipal_nr', 'orgnrs'])
+    return intersections_df
 
 if __name__ == '__main__':
-    print("Retrieving farmer elevation data, satellite data and polygon data")
-    farmer_elevation = get_farmer_elevation()
-    polygons = get_polygon_data()
 
-    sat_orgnr = np.array(get_combined_satellite_data())
-    farm_orgnr = np.array(list(farmer_elevation['orgnr']))
-    intersection = np.intersect1d(sat_orgnr, farm_orgnr)
-    
-    filtered_farmer_elevation = farmer_elevation[farmer_elevation['orgnr'].isin(intersection)]
-    filtered_satellite_data = intersection[:]
+    print("Fetching Data")
+    field_data, filtered_disp_eien = filter_data()
 
-    print(f"Farmer elevation shape: {filtered_farmer_elevation.shape}")
-    print(f"Length of satellite data after filtering: {len(filtered_satellite_data)}")
+    print("Extracting orgnrs per field")
+    intersections_df = extract_orgnr_per_field(field_data, filtered_disp_eien)
 
-    municipal_nrs_farm = np.array(list(set(filtered_farmer_elevation['commune_id'])))
-    municipal_nrs_sat = np.array(list(set(polygons['KOMID'])))
-    intersection = np.intersect1d(municipal_nrs_farm, municipal_nrs_sat)
-
-    filtered_farmer_elevation = filtered_farmer_elevation[filtered_farmer_elevation['commune_id'].isin(intersection)]
-    filtered_polygons = polygons[polygons['KOMID'].isin(intersection)]
-
-    print(f"Farmer elevation shape after second filtering: {filtered_farmer_elevation.shape}")
-    print(f"Polygon data shape after second filtering: {filtered_polygons.shape}")
-   
-    
-
-    municipal_nrs = list(set(filtered_farmer_elevation['commune_id']))
-    for municipal_nr in municipal_nrs:
-        print(municipal_nr)
-        orgnrs_by_municipal_nr = get_orgnrs_by_municipal(municipal_nr, filtered_farmer_elevation)
-        print(orgnrs_by_municipal_nr.shape)
-        polygons_by_municipal_nr = get_polygons_by_municipal(filtered_polygons, municipal_nr)
-        print(len(polygons_by_municipal_nr))
-        
-        # schema = {'geometry': 'Polygon'}
-        # with collection('shapefiles/' + str(municipal_nr) + '.shp', "w", crs=from_epsg(25833), driver="ESRI Shapefile", schema=schema) as output:
-        #     for polygon in tqdm(polygons_by_municipal_nr):
-        #         output.write({ 'geometry': mapping(polygon)})
-        
-        
-        row = orgnrs_by_municipal_nr.loc[orgnrs_by_municipal_nr['commune_id'] == municipal_nr].iloc[0]
-        bounding_box = boundingBox(row['lat'], row['lng'], 1)
-        
-        polygons_plt = [mapping(poly)['coordinates'][0][0] for poly in polygons_by_municipal_nr]
-        
-        for coords in polygons_plt:
-            for xy in coords:
-                print(xy)
-
-
-        #print(bounding_box)
-        transformer = Transformer.from_crs('epsg:25833', 'epsg:4326')
-        x0, y0 = transformer.transform(bounding_box[0], bounding_box[1])
-        x1, y1 = transformer.transform(bounding_box[2], bounding_box[3])
-
-        print(x0, y0, x1, y1)
-        #box = box(x0, y0, x1, y1)
-        box = box(bounding_box[0], bounding_box[1], bounding_box[2], bounding_box[3])
-
-        for p in polygons_plt:
-            plt.plot(*Polygon(p).exterior.xy)
-
-        plt.plot(*box.exterior.xy)
-        plt.show()
-        # for orgnr in list(orgnrs_by_municipal_nr['orgnr']):
-        #     #print(filtered_farmer_elevation.loc[filtered_farmer_elevation['orgnr'] == orgnr].iloc[0])
-        #     ...
-        #     break
-        break
+    print("Create csv from data")
+    intersections_df.to_csv(os.path.join(data_location, 'farm-information/orgnrs_per_field.csv'))
+    print(intersections_df.head())
+    print(intersections_df.shape)
     
--- src/data_handling/gdb_to_csv.py
+++ src/data_handling/gdb_to_csv.py
@@ -3,7 +3,7 @@ import pandas as pd
 import fiona
 from tqdm import tqdm
 import os
-
+import sys
 
 print("Reading jordsmonn_norge")
 layers = fiona.listlayers('../kornmo-data-files/raw-data/soil-data/jordsmonn.gdb')
@@ -13,6 +13,10 @@ for layer in tqdm(layers, total=len(layers)):
     new_gdf = gpd.read_file('../kornmo-data-files/raw-data/soil-data/jordsmonn.gdb', layer=layer)
     jordsmonn = pd.concat([jordsmonn, new_gdf])
 
+ids = range(0, jordsmonn.shape[0])
+jordsmonn.insert(0, "id", ids)
+jordsmonn = jordsmonn.rename(columns = {'KOMID': 'municipal_nr'})
+
 print("Reading jordkvalitet_norge.gdb")
 
 soilquality = gpd.read_file('../kornmo-data-files/raw-data/soil-data/soil-quality.gdb', driver='FileGDB', layer=0)
@@ -20,12 +24,15 @@ soilquality = soilquality.dropna()
 
 
 print("Creating jordsmonn for geometry and municipal_id")
-jordsmonn_geometry = jordsmonn[['KOMID', 'geometry']]
+jordsmonn_geometry = jordsmonn[['id', 'municipal_nr', 'geometry']]
 
 print("Removing columns")
 columns_to_remove = ['objtype', 'kartleggingsetappe', 'originaldatavert', 'områdeid', 'kopidato', 'navnerom', 'lokalid', 'geometry']
 df_for_models = soilquality.drop(columns=columns_to_remove)
 df_with_all = soilquality
+
+
+
 
 print("Converting to csv")
 df_for_models.to_csv(os.path.join('../kornmo-data-files/raw-data/soil-data', 'soilquality_refined.csv'))
