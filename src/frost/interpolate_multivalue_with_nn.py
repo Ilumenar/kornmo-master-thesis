12564b6fb0d463b53244ec8a208558794aff14d9
ilumenar <mikkelkvande96@hotmail.com>
2022-03-22 15:48:28 +0100

    Updated the file according to changes in single value interpolation

--- src/frost/interpolate_multivalue_with_nn.py
+++ src/frost/interpolate_multivalue_with_nn.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import numpy as np
 from utils import distance
@@ -6,17 +7,14 @@ import weather_interpolation_utils as wiu
 from tqdm import tqdm
 
 
-def get_number_of_days(readings):
-    n_days = 0
-    while True:
-        if any(f'day_{n_days}' in col_name for col_name in readings.columns):
-            n_days += 1
-        else:
-            return n_days
+weather_data_path = "../../../kornmo-data-files/raw-data/weather-data/"
 
 
 def get_k_closest_stations_multivalue(sensors: pd.DataFrame, distances: pd.DataFrame, lat, lng, masl, k: int):
     closest = distances.head(5 * k).merge(sensors, how='inner', left_on='id', right_on='station_id').head(k)
+
+    if len(closest) < k:
+        print(f"Could not find {k} stations: {closest}")
 
     series = pd.Series(dtype='float64')
     station_count = 0
@@ -32,40 +30,39 @@ def get_k_closest_stations_multivalue(sensors: pd.DataFrame, distances: pd.DataF
     return series
 
 
-def generate_interpolated_multivalue_for_year(growth_season, weather_feature, lower_bound, upper_bound):
+def generate_interpolated_multivalue_for_year(growth_season, weather_feature, lower_bound, upper_bound, starting_index):
 
     multivalue_model = load_model(f'nn_interpolation_models/{weather_feature}_model.h5')
     # Tensorflow outputs some garbage on the first use, which ruins the progress bars, so let's get it over with.
     multivalue_model.predict(np.zeros(shape=(1, 18)))
 
-    readings = pd.read_csv(f"../../../kornmo-data-files/raw-data/weather-data/processed/{weather_feature}/{weather_feature}_processed_{growth_season}-03-01_to_{growth_season}-10-01.csv")
-    sensors = pd.read_csv(f"../../../kornmo-data-files/raw-data/weather-data/frost_weather_sources.csv", index_col="id")[['lng', 'lat', 'masl']]
+    readings = pd.read_csv(os.path.join(weather_data_path, f"cleaned/{weather_feature}/{weather_feature}_cleaned_{growth_season}-03-01_to_{growth_season}-10-01.csv"))
+    sensors = pd.read_csv(os.path.join(weather_data_path, f"frost_weather_sources.csv"), index_col="id")[['lng', 'lat', 'masl']]
     readings = readings.join(sensors, "station_id")
     readings = readings.reset_index()
 
-    farmers = pd.read_csv(f"../../../kornmo-data-files/raw-data/farm-information/all-farmers-with-location.csv")[['orgnr', 'longitude', 'latitude', 'elevation']]
+    farmers = pd.read_csv(f"../../../kornmo-data-files/raw-data/farm-information/farmers-with-coordinates-and-soil_quality.csv")[['orgnr', 'longitude', 'latitude', 'elevation']]
 
     farmer_distances = {}
     p_bar = tqdm(farmers.iterrows(), total=len(farmers))
     p_bar.set_description("Calculating distances")
     for idx, farmer in p_bar:
         farmer_sensors = sensors.copy()
-        farmer_sensors['distance'] = sensors \
-            .apply(lambda ws: distance((farmer.latitude, farmer.longitude), (ws.lat, ws.lng)), axis=1)
+        farmer_sensors['distance'] = sensors.apply(lambda ws: distance((farmer.latitude, farmer.longitude), (ws.lat, ws.lng)), axis=1)
         farmer_sensors = farmer_sensors.sort_values(by=['distance'])
         farmer_distances[farmer.orgnr] = farmer_sensors[['distance']]
 
-    n_days = get_number_of_days(readings)
+    n_days = wiu.get_number_of_days(readings)
 
-    p_bar = tqdm(range(n_days))
+    p_bar = tqdm(range(starting_index, n_days))
     for day in p_bar:
-        p_bar.set_description_str(f"Interpolating {weather_feature} {growth_season}, day {day} of {n_days - 1}")
         readings_for_day = readings[["station_id", "lat", "lng", "masl", f"day_{day}_min", f"day_{day}_mean", f"day_{day}_max"]]
         readings_for_day = readings_for_day.rename(columns={
             f"day_{day}_min": "min",
             f"day_{day}_mean": "mean",
             f"day_{day}_max": "max",
         })
+
         nn_input = pd.DataFrame()
         for index, farmer in farmers.iterrows():
             closest_sensors = get_k_closest_stations_multivalue(
@@ -77,7 +74,7 @@ def generate_interpolated_multivalue_for_year(growth_season, weather_feature, lo
                 3
             )
 
-            nn_input = pd.concat([nn_input, closest_sensors.to_frame().T])
+            nn_input = pd.concat([nn_input, closest_sensors.to_frame().T], ignore_index=True)
 
         nn_input = wiu.normalize_multivalue_inputs(nn_input, lower_bound, upper_bound)
         nn_prediction = multivalue_model.predict(nn_input.to_numpy(), batch_size=len(nn_input))
@@ -85,6 +82,8 @@ def generate_interpolated_multivalue_for_year(growth_season, weather_feature, lo
         farmers[f'day_{day}_mean'] = wiu.denormalize_prediction(nn_prediction[1].flatten(), lower_bound, upper_bound)
         farmers[f'day_{day}_max'] = wiu.denormalize_prediction(nn_prediction[2].flatten(), lower_bound, upper_bound)
 
-    print('saving...')
-    farmers.to_csv(f"../../../kornmo-data-files/raw-data/weather-data/nn_interpolated/{weather_feature}/{weather_feature}_interpolated_{growth_season}-03-01_to_{growth_season}-10-01.csv", float_format='%.1f')
+        farmers.to_csv(os.path.join(weather_data_path, f"nn_interpolated/{weather_feature}/{weather_feature}_interpolated_{growth_season}-03-01_to_{growth_season}-10-01.csv"), float_format='%.1f')
+        p_bar.set_description_str(f"Done interpolating day {day} of {n_days - 1}")
+
+    farmers.to_csv(os.path.join(weather_data_path, f"nn_interpolated/{weather_feature}/{weather_feature}_interpolated_{growth_season}-03-01_to_{growth_season}-10-01.csv"), float_format='%.1f')
     print(f"Done with interpolating all values for {weather_feature} in {growth_season}")
