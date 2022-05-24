137fc5996e335888c4264457387b7f8739cf8389
ilumenar <mikkelkvande96@hotmail.com>
2022-05-24 11:34:23 +0200

    Removed username check

--- src/utils.py
+++ src/utils.py
@@ -12,11 +12,7 @@ import geopandas as gpd
 import os
 from shapely import wkt
 
-
-if os.getlogin() == "Mikkel":
-    data_location = "../../kornmo_old-data-files/raw-data"
-else:
-    data_location = "../../../kornmo_old-data-files/raw-data"
+data_location = "../../../kornmo_old-data-files/raw-data"
 
 
 # Groups a dataframe, gdf, by column, counts the values and plots it as a bar plot.
